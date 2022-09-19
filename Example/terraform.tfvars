0f9b966906e8c6a2b80b124028b530d32bd1678c
Ashukumar83 <ashuverma8321@gmail.com>
2022-09-19 15:31:09 +0530

    full fledged 6

--- Example/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-ins_name = ["Ak01" , "Ak02" ]
-ami = ["ami-09de362f44ba0a166" , "ami-05c8ca4485f8b138a" ]
--- main.tf
+++ main.tf
@@ -1,14 +1,23 @@
-resource "aws_instance" "web" {
-  ami           = var.ami[count.index]
-  instance_type = "t2.micro"
-  count = length(var.ami)
-
-  tags = {
-    Name = var.ins_name[count.index],
-    #Name = "demo-instance-ashu ${count.index}",
-    Owner = "Ashu Kumar"
-  }
+module "Example" {
+  source = "./Example"
+  
+  
 }
+
+
+
+
+# resource "aws_instance" "web" {
+#   ami           = var.ami[count.index]
+#   instance_type = "t2.micro"
+#   count = length(var.ami)
+
+#   tags = {
+#     Name = var.ins_name[count.index],
+#     #Name = "demo-instance-ashu ${count.index}",
+#     Owner = "Ashu Kumar"
+#   }
+# }
 
 
 
