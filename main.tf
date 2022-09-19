2959746f486f3f587e1fc5f59d1f97b0a6cee032
Ashukumar83 <ashuverma8321@gmail.com>
2022-09-19 15:42:47 +0530

    full fledged 9

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
-module "Example" {
-  source = "./Example"
+# module "Example" {
+#   source = "./Example"
   
   
-}
-
-
-
-
-# resource "aws_instance" "web" {
-#   ami           = var.ami[count.index]
-#   instance_type = "t2.micro"
-#   count = length(var.ami)
-
-#   tags = {
-#     Name = var.ins_name[count.index],
-#     #Name = "demo-instance-ashu ${count.index}",
-#     Owner = "Ashu Kumar"
-#   }
 # }
+
+
+
+
+resource "aws_instance" "web" {
+  ami           = var.ami[count.index]
+  instance_type = "t2.micro"
+  count = length(var.ami)
+
+  tags = {
+    Name = var.ins_name[count.index],
+    #Name = "demo-instance-ashu ${count.index}",
+    Owner = "Ashu Kumar"
+  }
+}
 
 
 
